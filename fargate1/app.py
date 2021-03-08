4baf6e1e26e135fd98c9989b05c209772d3c1e38
Teddy Fontaine <tf.sheol@gmail.com>
2021-03-08 01:30:11 +0100

    finish fargate2 & fix fargate1 (and make other stuffs)

--- fargate1/app.py
+++ fargate1/app.py
@@ -2,6 +2,7 @@ import configparser
 import json
 import boto3
 import requests
+import sentry_sdk
 
 
 def query_data(config, path, result, lambda_filter, lambda_pages, json_key='data', starting_after=''):
@@ -17,8 +18,11 @@ def query_data(config, path, result, lambda_filter, lambda_pages, json_key='data
     """
 
     response = requests.get(
-        config['Intercom']['api_endpoint'] + '/' + path,
-        params={'starting_after': starting_after},
+        '{}/{}'.format(config['Intercom']['api_endpoint'], path),
+        params={
+            'starting_after': starting_after,
+            'per_page': 50  # tmp fix ...
+        },
         headers={
             'Authorization': 'Bearer ' + config['Intercom']['access_token'],
             'Accept': 'application/json;charset=UTF-8'
@@ -47,14 +51,19 @@ def get_conversations(config):
                       'conversations')
 
 
-def store_in_s3(config, file_name, data):
-    print('store', file_name, 'in S3')
-    client = boto3.client(
+def init_s3(config):
+    return boto3.client(
         's3',
         aws_access_key_id=config['AWS']['access_key'],
         aws_secret_access_key=config['AWS']['secret'])
-    client.put_object(
-        Body=(bytes(json.dumps(data).encode('UTF-8'))), Bucket=config['S3']['datalake'], Key=config['S3'][file_name])
+
+
+def store_in_s3(config, s3, file_name, data):
+    print('store', file_name, 'in S3')
+    s3.put_object(
+        Body=(bytes(json.dumps(data).encode('UTF-8'))),
+        Bucket=config['S3']['datalake'],
+        Key=config['S3'][file_name])
 
 
 def import_intercom():
@@ -62,13 +71,18 @@ def import_intercom():
     config = configparser.ConfigParser()
     config.read('config.ini')
 
+    sentry_sdk.init(config['Sentry']['url'], traces_sample_rate=1.0)
+
     users = get_contacts(config)
     conversations = get_conversations(config)
 
     print(len(users), 'users')
     print(len(conversations), 'conversations')
-    store_in_s3(config, 'users', users)
-    store_in_s3(config, 'conversations', conversations)
+
+    s3 = init_s3(config)
+
+    store_in_s3(config, s3, 'users', users)
+    store_in_s3(config, s3, 'conversations', conversations)
 
 
 if __name__ == '__main__':
--- fargate2/app.py
+++ fargate2/app.py
@@ -1,7 +1,100 @@
+from datetime import datetime
+import configparser
+import json
+import boto3
+import mysql.connector
+import sentry_sdk
+
+
+def init_s3(config):
+    return boto3.client(
+        's3',
+        aws_access_key_id=config['AWS']['access_key'],
+        aws_secret_access_key=config['AWS']['secret'])
+
+
+def get_s3_data_as_a_json(config, s3, file_name):
+    response = s3.get_object(
+        Bucket=config['S3']['datalake'], Key=config['S3'][file_name])
+    return json.loads(response['Body'].read())
+
+
+def init_mysql_connection(config):
+    return mysql.connector.connect(
+        host=config['Datawarehouse']['host'],
+        user=config['Datawarehouse']['user'],
+        password=config['Datawarehouse']['password'],
+        database=config['Datawarehouse']['db']
+    )
+
+
+def truncate_sql_table(sql, table):
+    cursor = sql.cursor()
+    cursor.execute('TRUNCATE TABLE `' + table + '`')
+    cursor.close()
+
+
+def store_users_in_datawarehouse(sql, users):
+    """users
+
+    user_id	varchar(255) NULL	
+    name	varchar(255) NULL	
+    email	varchar(255) NULL	
+    """
+
+    cursor = sql.cursor()
+    for user in users:
+        print('insert in users table:',
+              user['id'], user['name'], user['email'])
+        cursor.execute('INSERT INTO `users` (user_id, name, email) VALUES ("{}", "{}", "{}")'
+                       .format(user['id'], user['name'], user['email']))
+    cursor.close()
+
+
+def store_conversations_in_datawarehouse(sql, conversations):
+    """conversation
+
+    conv_id	varchar(255) NULL	
+    user_id	varchar(255) NULL	
+    creation_date	datetime NULL	
+    body	longtext NULL
+    """
+
+    cursor = sql.cursor()
+    for conversation in conversations:
+        date_time = datetime.fromtimestamp(conversation['created_at'])
+        user_id = (0, conversation['contacts']['contacts'][0]['id'])[
+            len(conversation['contacts']['contacts']) > 0]
+        print('insert in conversations table:', conversation['id'], user_id,
+              date_time, conversation['source']['body'])
+        cursor.execute('INSERT INTO `conversation` (conv_id, user_id, creation_date, body) VALUES ("{}", "{}", "{}", "{}")'
+                       .format(conversation['id'], user_id, date_time, conversation['source']['body']))
+    cursor.close()
+
+
 def etl_datalake_to_datawarehouse():
-    # write your code here
-    
     print('etl_datalake_to_datawarehouse')
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    sentry_sdk.init(config['Sentry']['url'], traces_sample_rate=1.0)
+
+    s3 = init_s3(config)
+
+    users = get_s3_data_as_a_json(config, s3, 'users')
+    conversations = get_s3_data_as_a_json(config, s3, 'conversations')
+
+    sql = init_mysql_connection(config)
+
+    truncate_sql_table(sql, 'users')
+    truncate_sql_table(sql, 'conversation')  # error in database (missing 's')
+
+    store_users_in_datawarehouse(sql, users)
+    store_conversations_in_datawarehouse(sql, conversations)
+
+    sql.commit()
+    sql.close()
+
 
 if __name__ == '__main__':
     etl_datalake_to_datawarehouse()
